358f2bb48c7ecf1fb0b540f5613807ea2bcf9537
CatRass <catrassyt@gmail.com>
2023-06-23 17:34:28 +1000

    Several Changes: New Packages, New Pages and more - Installed Browserify - Installed Browserify-FS - Added a new page "subpage"

--- server.js
+++ server.js
@@ -1,34 +1,19 @@
-// const http = require('http');
-// const express = require("express");
-// const ejs = require('ejs');
-
-// const hostname = '127.0.0.1';
-// const port = 3000;
-// const app = express()
-
-// app.listen(3000, () => { 
-//     console.log("Server running on port 3000");
-// });
-
-// app.get("/", (req,res) => { 
-//     // app.get to handle GET requests
-//     // req - http request, res - desired response
-//     res.send("Hello World"); // send Hello World to this route
-// });
-
 var express = require('express');
+const browserify = require('browserify');
+const fs = require('fs');
+var app = express();   
+
 const ejs = require('ejs');
-// Initialise Express
-var app = express();
-// Render static files
-app.use(express.static('public'));
-// Set the view engine to ejs
+
+app.use(express.static('public/'));
 app.set('view engine', 'ejs');
-// Port website will run on
-app.listen(3000);
+app.listen(1186);
 
 // *** GET Routes - display pages ***
 // Root Route
 app.get('/', function (req, res) {
     res.render('pages/index');
+});
+app.get('/subpage', function (req, res) {
+    res.render('pages/subpage');
 });
