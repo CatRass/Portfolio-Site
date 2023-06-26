358cc7c1e08d07120faa5b3915c67b86996d8011
CatRass <catrassyt@gmail.com>
2023-06-26 21:33:48 +1000

    Added IGDB API and Small changes - Set up Twitch Developer Authentication - Setup IGDB API connection - Small changes to main.css - Connected partial 'navbar' to index.ejs - Connected partial 'navbar' to subpage.ejs - Connected main.css to header.ejs - Created partial navbar.ejs

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-var express = require('express');
-const browserify = require('browserify');
-const fs = require('fs');
-var app = express();   
-
+const express = require('express');
+const app = express();  
 const ejs = require('ejs');
+const twitchCreds = require('./tokens/twitchCreds.json');
+
+const fetch = require('node-fetch');
+const fs = require('fs');
 
 app.use(express.static('public/'));
 app.set('view engine', 'ejs');
@@ -17,3 +18,40 @@ app.get('/', function (req, res) {
 app.get('/subpage', function (req, res) {
     res.render('pages/subpage');
 });
+
+// ======================================================================
+
+const client_id = twitchCreds.client_id;
+const client_secret = twitchCreds.client_secret;
+
+async function getTwitchAccessToken(client_id, client_secret){
+
+    const response = await fetch('https://id.twitch.tv/oauth2/token?client_id='+client_id+'&client_secret='+client_secret+'&grant_type=client_credentials', {
+        method: 'post',
+    });
+
+    const data = await response.json();
+    console.log(data['access_token']);
+    return data['access_token'];
+    
+}
+
+
+
+async function getGaming(twitchAuth, client_id){
+
+    const response = await fetch('https://api.igdb.com/v4/games', {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Client-ID': client_id,
+            'Authorization': "Bearer " + await twitchAuth
+        },
+        body: 'fields name, involved_companies; search "Tears of the kingdom";'
+    });
+    const data = await response.json();
+    console.log(data);
+}
+
+var token = getTwitchAccessToken(client_id, client_secret);
+var games = getGaming(token,client_id);
